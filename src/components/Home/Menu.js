ea6bdcb866fbdd14f85cfa976444960853d46839
Jimmy <jajalaba@yahoo.com.tw>
2020-05-26 15:24:53 +0800

    commpont and page cover

---
+++ src/components/Home/Menu.js
@@ -0,0 +1,116 @@
+import React, { Component } from "react"
+import Title from "../Globals/Title"
+import Img from "gatsby-image"
+
+const getCategories = items => {
+  let tempItems = items.map(items => {
+    return items.node.category
+  })
+  let tempCategories = new Set(tempItems)
+  let categories = Array.from(tempCategories)
+  categories = ["全部", ...categories]
+  return categories
+}
+export default class Menu extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: props.items.edges,
+      productItems: props.items.edges,
+      categories: getCategories(props.items.edges),
+    }
+  }
+
+  handleItems = category => {
+    let tempItems = [...this.state.items]
+    if (category === "全部") {
+      this.setState(() => {
+        return { productItems: tempItems }
+      })
+    } else {
+      let items = tempItems.filter(({ node }) => node.category === category)
+      this.setState(() => {
+        return { productItems: items }
+      })
+    }
+  }
+  render() {
+    if (this.state.items.length > 0) {
+      return (
+        <section className="menu py-5">
+          <div className="container">
+            <Title title="產品分類" />
+            {/* categories */}
+            <div className="row mb-5">
+              <div className="col-10 mx-auto text-center">
+                {this.state.categories.map((category, index) => {
+                  return (
+                    <button
+                      type="button"
+                      key={index}
+                      className="btn btn-yellow m-3"
+                      onClick={() => {
+                        this.handleItems(category)
+                      }}
+                    >
+                      {category}
+                    </button>
+                  )
+                })}
+              </div>
+            </div>
+            {/* items */}
+            <div className="row">
+              {this.state.productItems.map(({ node }) => {
+                return (
+                  <div
+                    key={node.id}
+                    className="col-11 col-md-6 my-3 d-flex mx-auto"
+                  >
+                    <div>
+                      <Img fixed={node.image.fixed} />
+                    </div>
+                    {/* 產品說明 */}
+                    <div className="flex-grow-1 px-3">
+                      <div className="d-flex justify-content-between">
+                        <h6 className="mb-0">
+                          <small>{node.title}</small>
+                        </h6>
+                        <h6 className="mb-0 text-yellow">
+                          <small>${node.price}</small>
+                        </h6>
+                      </div>
+                      <a
+                        href={node.description.description}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        立即前往
+                      </a>
+                      {/* <p className="text-muted">
+                        <small>{node.description.description}</small>
+                      </p> */}
+                    </div>
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+        </section>
+      )
+    } else {
+      return (
+        <section className="menu py-5">
+          <div className="container">
+            <Title title="我們最好的商品"></Title>
+            <div className="row">
+              <div className="col-10 col-sm-6 mx-suto text-center text-capitalize">
+                <h1>目前沒有商品可以顯示</h1>
+              </div>
+            </div>
+          </div>
+        </section>
+      )
+    }
+  }
+}
--- src/pages/404.js
+++ src/pages/404.js
@@ -10,5 +10,6 @@ const NotFoundPage = () => (
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
 )
+// 404
 
 export default NotFoundPage
